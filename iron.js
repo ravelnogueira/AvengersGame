639079f6b977c4e81c023f536bf9d1bba9e25b14
nogueiraravel <88169028+nogueiraravel@users.noreply.github.com>
2021-08-19 10:42:17 -0300

    Update iron.js
    Inserindo uma function ao invés de ter somente o while

--- iron.js
+++ iron.js
@@ -1,19 +1,17 @@
-var escolher ;
-while (true) {
-    escolher =prompt("1 - Recusar de desenvolver o missel // 2 - Aceitar a desenvolver, e solicitar ferramentas e materias belícos")
-
-    if (escolher ==1 ) {
-        
-        location.replace ('gameover1.html')
-        
-        break
-    }       
-
-    else if (escolher ==2 ) {
-         break
-    }
-
-    else {
-        alert("Você digitou errado")        
-    }
-}
+function escolha(){ 
+    var escolha = ""
+            while (escolha != 1 && escolha != 2) {
+              escolha = prompt("1 - Recusar de desenvolver o missel. \n 2 - Aceitar a desenvolver, e solicitar ferramentas e insumos")
+            
+            if (escolha == 1) {
+            
+              window.location.href = "gameover1.html"
+            
+            } else if (escolha == 2) {
+            
+              window.location.href = "iron2.html"
+            }
+  
+            else {alert('Escolha incorreta')} 
+            }
+        }
